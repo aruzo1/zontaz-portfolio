08d03d45db1814f6b2244c8542d4b4d4dd5533a3
aruzo <dawid12391111@gmail.com>
2023-11-07 16:37:16 +0100

    add social links

--- components/footer/constants.tsx
+++ components/footer/constants.tsx
@@ -2,15 +2,15 @@ import { FiverrIcon, GithubIcon, InstagramIcon } from "@/components";
 
 export const SOCIALS = [
   {
-    href: "https://www.facebook.com/",
+    href: "https://www.instagram.com/zontaz1/",
     icon: <InstagramIcon className="w-6 h-6" />,
   },
   {
-    href: "https://www.facebook.com/",
+    href: "https://github.com/zontaz-dev",
     icon: <GithubIcon className="w-6 h-6" />,
   },
   {
-    href: "https://www.facebook.com/",
+    href: "https://www.fiverr.com/aruzoyt",
     icon: <FiverrIcon className="w-5 h-5" />,
   },
 ];
