9a86ab9b046336a28916abe6dd403fe7af86a4e0
Dawid <dawid12391111@gmail.com>
2025-03-26 17:39:41 +0100

    Fix imports

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  transpilePackages: [
+    "@radix-ui/react-dialog",
+    "@radix-ui/react-accordion",
+    "@radix-ui/react-toast",
+  ],
 };
 
 module.exports = nextConfig;
